1c1d3936f740eb2c77cb1d659622c4e22bfff750
psjw <dlrudtn108@gmail.com>
2025-03-14 15:09:59 +0900

    section2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,52 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
         //code
+        try {
+            //1. Member 추가
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
 
-        em.close();
+            //2. Member 조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+
+            //3. Member 삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+
+            //4. Member 수정 - em.persist 호출안해도됨
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            //5. JPQL 멤버 객체를 대상으로 질의
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name = "USER")
+public class Member {
+    @Id
+    private Long id;
+//    @Column(name = "username")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
